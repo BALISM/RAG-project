4f35d8bafef506a4eeade80921ca68f11f9f9bfa
BALISM <muhammadbalajsheikh173@gmail.com>
2026-07-29 17:00:56 +0500

    new test file created

--- test/test_ingestion.py
+++ test/test_ingestion.py
@@ -223,4 +223,4 @@ def test_chunk_document_raises_on_unsupported_type(tmp_path):
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main([__file__, "-v"]))
+    sys.exit(pytest.main([__file__, "-v"])) #first test passed with 18 items
