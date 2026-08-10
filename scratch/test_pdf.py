be108ca41ec6600a1e8c669760458d0206927ff1
BALISM <muhammadbalajsheikh173@gmail.com>
2026-08-10 19:37:58 +0500

    introducing pdf abt the project

---
+++ scratch/test_pdf.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from pathlib import Path
+
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.platypus import HRFlowable, KeepTogether, PageBreak, Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle
+
+print("Initializing PDF Generator...")
